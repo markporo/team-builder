8fa3498507f379d92ce9f2bfe4d05a3bb54a1352
markporo <markporo@yahoo.com>
2021-08-09 03:28:51 -0400

    so close to having a funcitoning edit command

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css';
 import TeamMemberForm from './components/TeamMemberForm';
+//import Button from "/.components/Button"
 
 
 // default form values
@@ -13,6 +14,9 @@ function App() {
   // what would be the stat that holds the database array of the team
   const [team, setTeamMembers] = useState([]);
 
+  //boolean - in current edit
+  const [currentlyInEdit, setIsInCurrentEdit] = useState(false);
+
   //update the formValues of the instance of the user adding their info
   function updateForm(inputName, inputValue) {
 
@@ -21,32 +25,49 @@ function App() {
 
   // the submitForm function that submits data from the form to post
   const submitForm = e => {
+
     // acquire info from form
     const newTeamMember = { name: formValues.name.trim(), role: formValues.role.trim(), email: formValues.email.trim() };
 
     // safeguard against updating form with not all inputs filled
     // I used required on inputs
 
-    // update by sending old infor and new info to database with axios
-    setTeamMembers([...team, newTeamMember]);
+    if (currentlyInEdit === true) {
+      team.map(member => {
+        console.log(member, "member that is in edit")
+        let updatedMember;
+        if (member.name === newTeamMember.name || member.email === newTeamMember.email) {
+          updatedMember = newTeamMember;
+        }
+        return ([...team, updatedMember])
+      })
+
+    } else
+
+      // update by sending old infor and new info to database with axios
+      setTeamMembers([...team, newTeamMember]);
 
     //clear formValues
     setFormValues(defaultFormValues);
+    setIsInCurrentEdit(false);
   }
-
-
-
 
 
   return (
     <div className="App">
       <h1>Our Team List</h1>
       <div>
-        {team.map(eachMember => <p>{`Name: ${eachMember.name} Role: ${eachMember.role} Email: ${eachMember.email}`}</p>)}
+        {team.map(eachMember =>
+          <div>
+            <p>{`Name: ${eachMember.name} Role: ${eachMember.role} Email: ${eachMember.email}`}
+            </p>
+            <button onClick={(e) => { setIsInCurrentEdit(!currentlyInEdit); currentlyInEdit ? setFormValues(eachMember) : setFormValues(defaultFormValues); }}>edit
+            </button>
+          </div>)}
       </div>
       <TeamMemberForm formValues={formValues} submitForm={submitForm} updateForm={updateForm} />
     </div>
-  );
+  )
 }
 
 export default App;
--- team-builder/src/components/TeamMemberForm.js
+++ team-builder/src/components/TeamMemberForm.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  text-align: center;
+  color: black;
+  backgroundColor: white;
+  border: 1px solid black;
+  width: 200px;
+  height: 50px;
+`;
+
+const Input = styled.input`
+  width: 80%;
+  margin: 0 auto;
+  height: 40px;
+  margin-bottom: 1rem;
+`;
+
+const Select = styled.select`
+  width: 80%;
+  margin: 0 auto;
+  height: 40px;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
 
 
-export default function TeamMemberForm({ formValues, submitForm, updateForm }) {
+
+export default function TeamMemberForm({ formValues, submitForm, updateForm, }) {
   //submit Handler for Form - posts form to databas
   function submitHandler(e) {
     e.preventDefault();
@@ -17,27 +43,28 @@ export default function TeamMemberForm({ formValues, submitForm, updateForm }) {
     <form onSubmit={submitHandler}>
       {/* we want this to be a controlled form */}
       <div>
-        <label> NAME:
-          <input type="text" name="name" value={formValues.name} required="true" onChange={changeHandler} />
-        </label>
+        <div>NAME:</div>
+        <Input type="text" name="name" value={formValues.name} required="true" onChange={changeHandler} />
+
       </div>
       <div>
-        <label>JOB ROLE:
-          <select style={{ backgroundColor: "yellow", color: 'black' }} value={formValues.role} name="role" required="true" onChange={changeHandler} >
-            <option value="FE Developer">FE Developer</option>
-            <option value="BE Developer">Back End Developer</option>
-            <option value="Cyber Security">Cyber Security</option>
-            <option value="Hype Man">Hype Man</option>
-            <option value="Coffee Maker">Coffee Maker</option>
-          </select>
-        </label>
+        <div>JOB ROLE:</div>
+        <Select value={formValues.role} name="role" required="true" onChange={changeHandler} >
+          <option value="----Choose a Role----">----Choose a Role----</option>
+          <option value="FE Developer">FE Developer</option>
+          <option value="BE Developer">BE Developer</option>
+          <option value="Cyber Security">Cyber Security</option>
+          <option value="Hype Man">Hype Man</option>
+          <option value="Coffee Maker">Coffee Maker</option>
+        </Select>
+
       </div>
       <div>
-        <label>EMAIL:
-          <input type="email" name="email" value={formValues.email} required="true" onChange={changeHandler} />
-        </label>
+        <div>EMAIL:</div>
+        <Input type="email" name="email" value={formValues.email} required="true" onChange={changeHandler} />
+
       </div>
-      <button>Submit</button>
+      <Button>Submit</Button>
     </form>
   )
 
